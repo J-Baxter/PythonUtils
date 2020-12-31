71d76e205a80f9c1672e935f534e3638030114cf
J-Baxter <james.baxter@ed.ac.uk>
2020-12-31 11:22:04 +0000

    skeleton output function

--- PubMedSearch2CSV.py
+++ PubMedSearch2CSV.py
@@ -82,6 +82,99 @@ def search_medline(idlist):
     return records
 
 
+def reformat_records(records):
+    aid_array = []
+    dp_array = []
+    ti_array = []
+    pmid_array = []
+    com_array = []
+    au_array = []
+    ab_array = []
+
+    for record in records:
+        pmid_array.append(record.get("PMID", np.nan))
+        aid_array.append(record.get("AID", np.nan))
+        dp_array.append(record.get("DP", np.nan))
+        au_array.append(record.get("AU", np.nan))
+        ti_array.append(record.get("TI", np.nan))
+        com_array.append(record.get("EIN|EFR|CRI|CFR|ECI|ECF|RPI|RPF|RIN|ROF", 'none'))
+        ab_array.append(record.get("AB", np.nan))
+
+    pmid_array = removeNA(pmid_array)
+    aid_array = removeNA(aid_array)
+    dp_array = removeNA(dp_array)
+    au_array = removeNA(au_array)
+    ti_array = removeNA(ti_array)
+    com_array = removeNA(com_array)
+    ab_array = removeNA(ab_array)
+
+    YE_Array = []
+    FAU_Array = []
+    DOI_Array = []
+
+    # Select YOP only
+    for i in range(len(dp_array)):
+        DP = dp_array[i]
+        year = int(str(DP)[0:4])
+        YE_Array.append(int(year))
+
+    # select DOI only
+    for i in range(len(aid_array)):
+        link = findDOI(aid_array[i])
+        DOI_Array.append(str(link))
+
+    # select first author only
+    for i in range(len(au_array)):
+        AU = au_array[i]
+        FAU = AU[0]
+        FAU_Array.append(str(FAU))
+
+
+
+    return
+
+
+def output():
+    print("\n", "Saving results to file...")
+
+    # write csv containing key information (title author Id etc)
+    df = pd.DataFrame(zip(pmid_array, ti_array, FAU_Array, YE_Array, DOI_Array, com_array))
+    df.columns = ["PMID", "Title", "First Author", "Publication Year", "DOI", "Observations"]
+
+    filename = "queryresults_" + d1 + ".csv"
+    df.to_csv(filename, index=False)
+
+    # write abstracts to formatted text file, annotated with title, first author year and ID tags
+    filename2 = "searchabstracts" + d1 + ".txt"
+
+    file = open(filename2, "w+")
+    for i in range(DOI_Array.__len__()):
+        file.write(TI_Array[i])
+        file.write('\n')
+        file.write('\n')
+        file.write(FAU_Array[i])
+        file.write(' ')
+        file.write(str(YE_Array[i]))
+        file.write('\n')
+        file.write('\n')
+        file.write(AB_Array[i])
+        file.write('\n')
+        file.write('\n')
+        file.write('PMID: ')
+        file.write(str(PMID_Array[i]))
+        file.write('\n')
+        file.write('DOI: ')
+        file.write(DOI_Array[i])
+        file.write('\n')
+        file.write('\n')
+        file.write('\n')
+        file.write('-----------------------------')
+        file.write('\n')
+    file.close
+
+    print("\n", "Done.")
+
+
 def main():
     # define arguments for PubMed search from terminal
     parser = argparse.ArgumentParser()
@@ -106,86 +199,6 @@ def main():
 
 
 # export into pandas dataframe, then export to csv
-AID_Array = []
-DP_Array = []
-TI_Array = []
-PMID_Array = []
-COM_Array = []
-AU_Array = []
-AB_Array = []
 
-for record in records:
-    PMID_Array.append(record.get("PMID", np.nan))
-    AID_Array.append(record.get("AID", np.nan))
-    DP_Array.append(record.get("DP", np.nan))
-    AU_Array.append(record.get("AU", np.nan))
-    TI_Array.append(record.get("TI", np.nan))
-    COM_Array.append(record.get("EIN|EFR|CRI|CFR|ECI|ECF|RPI|RPF|RIN|ROF", 'none'))
-    AB_Array.append(record.get("AB", np.nan))
 
-PMID_Array = removeNA(PMID_Array)
-AID_Array = removeNA(AID_Array)
-DP_Array = removeNA(DP_Array)
-AU_Array = removeNA(AU_Array)
-TI_Array = removeNA(TI_Array)
-COM_Array = removeNA(COM_Array)
-AB_Array = removeNA(AB_Array)
 
-YE_Array = []
-FAU_Array = []
-DOI_Array = []
-
-# Select YOP only
-for i in range(len(DP_Array)):
-    DP = DP_Array[i]
-    year = int(str(DP)[0:4])
-    YE_Array.append(int(year))
-
-# select DOI only
-for i in range(len(AID_Array)):
-    link = findDOI(AID_Array[i])
-    DOI_Array.append(str(link))
-
-# select first author only
-for i in range(len(AU_Array)):
-    AU = AU_Array[i]
-    FAU = AU[0]
-    FAU_Array.append(str(FAU))
-
-print("\n", "Saving results to file...")
-
-df = pd.DataFrame(zip(PMID_Array, TI_Array, FAU_Array, YE_Array, DOI_Array, COM_Array))
-
-df.columns = ["PMID", "Title", "First Author", "Publication Year", "DOI", "Observations"]
-
-filename = "queryresults_" + d1 + ".csv"
-df.to_csv(filename, index=False)
-
-filename2 = "searchabstracts" + d1 + ".txt"
-
-file = open(filename2, "w+")
-for i in range(DOI_Array.__len__()):
-    file.write(TI_Array[i])
-    file.write('\n')
-    file.write('\n')
-    file.write(FAU_Array[i])
-    file.write(' ')
-    file.write(str(YE_Array[i]))
-    file.write('\n')
-    file.write('\n')
-    file.write(AB_Array[i])
-    file.write('\n')
-    file.write('\n')
-    file.write('PMID: ')
-    file.write(str(PMID_Array[i]))
-    file.write('\n')
-    file.write('DOI: ')
-    file.write(DOI_Array[i])
-    file.write('\n')
-    file.write('\n')
-    file.write('\n')
-    file.write('-----------------------------')
-    file.write('\n')
-file.close
-
-print("\n", "Done.")
